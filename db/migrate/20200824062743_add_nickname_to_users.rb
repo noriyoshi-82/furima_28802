b8ee6ceece099123f606b3e3c80a1b5f0ef8372b
noriyoshi-82 <tomoyoshi.tomo1111@gmail.com>
2020-08-24 21:07:56 +0900

    ユーザー登録、ログイン機能実装

--- db/migrate/20200824062743_add_nickname_to_users.rb
+++ db/migrate/20200824062743_add_nickname_to_users.rb
@@ -5,6 +5,6 @@ class AddNicknameToUsers < ActiveRecord::Migration[6.0]
     add_column :users, :last_name,       :string, null: false
     add_column :users, :first_name_kana, :string, null: false
     add_column :users, :last_name_kana,  :string, null: false
-    add_column :users, :date,            :date,   null: false
+    add_column :users, :birthday,            :date,   null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2020_08_24_065551) do
     t.string "last_name", null: false
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
-    t.date "date", null: false
+    t.date "birthday", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
