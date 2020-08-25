b19ccd5981e7a0d21d37534b4d4a223a6a27042e
noriyoshi-82 <tomoyoshi.tomo1111@gmail.com>
2020-08-25 15:58:44 +0900

    ログイン画面エラー文表示機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :encrypted_password])
-
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :email, :password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
+
+  validates :password, format: { with: /\A[a-z0-9]+\z/i, message: "Include both letters and numbers"}
+  with_options presence: true do
+    validates :nickname
+    validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    validates :last_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    validates :birthday
+  end
 end
