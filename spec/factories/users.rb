a0bf86e4c737bbc1b39bd8bd2be4c49269bdaf9c
noriyoshi-82 <tomoyoshi.tomo1111@gmail.com>
2020-08-25 19:20:02 +0900

    Rubocopを実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 4)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    first_name            {"田中"}
-    last_name             {"大輔"}
-    first_name_kana       {"タナカ"}
-    last_name_kana        {"ダイスケ"}
-    birthday              {Faker::Date.backward}
+    nickname              { Faker::Name.initials(number: 4) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    first_name            { '田中' }
+    last_name             { '大輔' }
+    first_name_kana       { 'タナカ' }
+    last_name_kana        { 'ダイスケ' }
+    birthday              { Faker::Date.backward }
   end
 end
