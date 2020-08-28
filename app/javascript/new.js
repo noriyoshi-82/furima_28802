540712696d0dcb7919e61ab6b86efe6b0f618177
noriyoshi-82 <tomoyoshi.tomo1111@gmail.com>
2020-08-28 18:19:39 +0900

    JavaScript販売価格計算機能実装

---
+++ app/javascript/new.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function(){
+  const priceArea = document.getElementById("item-price")
+  priceArea.addEventListener('input', function(){
+    const price = document.getElementById("item-price").value; 
+    charge = price * 0.1;
+    profit = price - charge;
+
+    chargeArea = document.getElementById("add-tax-price");
+    profitArea = document.getElementById("profit");
+
+    chargeArea.textContent = Math.floor(charge);
+    profitArea.textContent = Math.floor(profit);
+  });
+});
+
