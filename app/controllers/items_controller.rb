69591678bbb9760797461738a6157dd6443d9b1c
noriyoshi-82 <tomoyoshi.tomo1111@gmail.com>
2020-08-31 18:17:42 +0900

    商品情報編集機能　実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -18,6 +18,24 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+  
   private
 
   def item_params
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :delivery_pay
-  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :prefectures
   belongs_to_active_hash :delivery_day
   belongs_to :user
   has_one :order
