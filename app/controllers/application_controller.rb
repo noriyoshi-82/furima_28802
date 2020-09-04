93f5896a91ff7e8de3edd757d629e30dda453647
noriyoshi-82 <tomoyoshi.tomo1111@gmail.com>
2020-09-04 21:51:59 +0900

    Bacis認証修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth
-  
+
   private
 
   def configure_permitted_parameters
@@ -12,5 +12,6 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
